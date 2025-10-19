cbe5807659f7d5f03d6a9de3097f171a41283aab
Denis Shakalo <aditor@gmail.com>
2025-10-19 11:29:07 +0300

    k8s-apps added
    k8s-apps added

---
+++ dev/k8s-apps/terraform.tfvars
@@ -0,0 +1,9 @@
+argocd_chart_version     = "8.2.5"
+appsets_chart_version    = "0.1.10"
+appset_name              = "it-delivers-everywhere-features"
+github_org               = "mdefenders"
+app_chart_repo           = "https://mdefenders.github.io/helmcharts"
+app_chart_name           = "service-template"
+app_chart_version        = "0.1.*"
+deploy_appsets           = true
+autoscaler_chart_version = "9.52.0"
--- dev/k8s/output.tf
+++ dev/k8s/output.tf
@@ -1,3 +1,15 @@
-output "eks" {
-  value = module.eks
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+output "cluster_certificate_authority_data" {
+  value     = module.eks.cluster_certificate_authority_data
+  sensitive = true
+}
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_autoscaler_irsa_role" {
+  description = "IAM Role ARN for Cluster Autoscaler Service Account"
+  value       = module.cluster_autoscaler_irsa_role.arn
 }
